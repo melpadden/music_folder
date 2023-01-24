92754c245c41e2ffab79b36c8523aed9ce504581
Mel Padden <mel.padden@outlook.com>
2023-01-24 13:35:21 +0100

    Initialise first template

---
+++ reorg.py
@@ -0,0 +1,17 @@
+folders = []
+artists = []
+albums = []
+
+def store_artists():
+    """Store all the artists in the folder"""
+    pass
+def generate_list():
+    """
+        generate a songlist.json describing the contents of the folder
+    """
+    pass
+def main():
+    store_artists()
+
+if  __name__ == "__main__":
+    main()
